c9956147db5170de847ebf0435f7c14705be57e6
ohorikazuya <ohorikazuya@team-lab.com>
2022-04-05 22:23:49 +0900

    functions

--- c_project/main.c
+++ c_project/main.c
@@ -1,11 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+double avg(double l, double m) {
+  double r = (l + m) / 2.0;
+  return r;
+}
+
 int main() {
-  int i = 1;
-  do {
-    printf("%d", i);
-    i++;
-  } while (i <= 5);
-  printf("\n");
+  double d1, d2, d3, d4;
+  double a = 1.2, b = 3.4, c = 2.7;
+
+  d1 = avg(a, b);
+  d2 = avg(4.1, 5.7);
+  d3 = avg(c, 2.8);
+  d4 = avg(2.0, 8.0);
+  printf("d1 = %f, d2 = %f, d3 = %f, d4 = %f\n", d1, d2, d3, d4);
 }
